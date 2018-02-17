333509c3551d0b771bb3962ca46e22635415f4ec
Yongha Hwang <yongha.hwang@gmail.com>
2018-02-16 22:50:36 -0500

    fix path

--- src/train_start.sh
+++ src/train_start.sh
@@ -1,7 +1,7 @@
 # move to your tensorflow object detection directory
-cd ~/repo/gsv2pv/models/research
+cd ~/repo/tensorflow/models/research
 # change parameters accordingly
 python object_detection/train.py \
 --logtostderr \
---pipeline_config_path=/home/ubuntu/repo/bike-count/data/training/config/faster_rcnn_resnet101_coco.config \
---train_dir=/home/ubuntu/repo/bike-count/data/training/train
+--pipeline_config_path=/home/ubuntu/repo/bike-detector/data/training/config/faster_rcnn_resnet101_coco.config \
+--train_dir=/home/ubuntu/repo/bike-detector/data/training/train
